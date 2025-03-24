5b9b0e9694a5645c309e4f2358ebde3668cde825
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-03-24 12:14:48 +0300

    CustomService.java

--- CustomService.java
+++ CustomService.java
@@ -1,15 +1,13 @@
 package ru.netology.service;
-import org.aleks.perov.Main;
-
-import java.util.Scanner;
 
 public class CustomService {
-
-     public static final int calculateCustoms(int price, int weight) {
-          int tax = price / 100 + weight  * 100;
+     public static final int COEFFICIENT_WEIGHT = 100;
+     public static int calculateCustoms(int price, int weight) {
+          int tax = price / 100 + weight  * COEFFICIENT_WEIGHT;
           return tax;
      }
 }
+
 
 
 
